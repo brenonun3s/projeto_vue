c2d3d785505721d0bf5b5d300f255168bd6231ec
brenonun3s <morais.brenonunes@hotmail.com>
2025-08-20 14:57:27 -0300

    First commit, versioning the initial project

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+  <h1>Olá Vue</h1>
+  <p> Meu nome é {{ name }} e trabalho como {{ profissao}}</p>
+ </div>
+
+</template>
+
+<script>
+ export default {
+   name:'PrimeiroComponente',
+ 
+ data() {
+  return {
+   name:"Breno",
+   profissao: "Desenvolvedor"
+  }
+ },
+  }
+   </script>
+
+
