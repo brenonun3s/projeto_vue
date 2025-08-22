faa6c7b7037f445b8a53327d1f37d37952e44ff0
brenonun3s <morais.brenonunes@hotmail.com>
2025-08-22 15:25:39 -0300

    Started studying Vue Directives (v-if, v-else, v-show and v-bind) and also dynamic attributes in Vue.js

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,20 +1,32 @@
 <template>
  <div>
-  <p>Estou trabalhando no momento.</p>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou buscando oportunidades</p>
   <p>Utilizo as seguintes tecnologias:</p>
   <ul>
    <li>JavaScript</li>
    <li>PHP</li>
    <li>Python</li>
   </ul>
+  <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+  <p>Para acessar meu portfólio <a :href="meu_link" target="_blank"> basta clicar aqui</a></p>
  </div>
 
 </template>
 <script>
+import Picture from './Picture.vue';
+
 export default {
  name: 'Info',
+ components: {
+  Picture
+ },
  data() {
   return {
+   esta_trabalhando: true,
+   mostrar_email: true,
+   email: 'morais.brenonunes@hotmail.com',
+   meu_link: "https://www.google.com/"
 
   }
  }
